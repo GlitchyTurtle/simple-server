c76af97cb7071c90d48d0aeae412f8377f50d4a4
GlitchyTurtle <93545476+GlitchyTurtle@users.noreply.github.com>
2023-05-03 17:35:14 -0600

    Create index.js

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+// Middleware to parse JSON request bodies
+app.use(express.json());
+
+// Define a GET route
+app.get('/api/data', (req, res) => {
+  const data = { message: 'Hello from the server!' };
+  res.json(data);
+});
+
+// Define a POST route
+app.post('/api/data', (req, res) => {
+  const receivedData = req.body;
+  // Process the data and send back a response
+  const responseData = { message: 'Data received successfully!' };
+  res.json(responseData);
+});
+
+// Serve static files from the "public" directory
+app.use(express.static('public'));
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server is listening on port ${port}`);
+});
